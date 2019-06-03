dc37468765ff8e560835cc645fa1ced27ea2a475
jawad727 <jawaddhussein@gmail.com>
2019-06-03 14:58:33 -0700

    PR for review

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,19 +8,23 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE 
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
+x = x + y
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
-# YOUR CODE HERE 
+# YOUR CODE HERE
+
+x.remove(8) 
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
-print(x)
+
 
 # Print the length of list x
 # YOUR CODE HERE 
